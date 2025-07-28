e82dfeddfbd669946f2886e944c9f6e6c652c58e
Marianna Ojanen <marianna.ojanen@kotiposti.net>
2025-07-28 15:13:00 +0300

    Add exercises 10.17-10.18

---
+++ rate-repository-app/src/components/RepositoryView.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { FlatList, View} from 'react-native'
+import { useParams } from 'react-router-native'
+import { useQuery } from '@apollo/client'
+import { GET_REPOSITORY } from '../graphql/queries'
+import RepositoryItem from '../components/RepositoryItem'
+import ReviewItem from './ReviewItem'
+import Text from './Text'
+
+const RepositoryView = () => {
+  const { id } = useParams();
+  const { data, loading, error } = useQuery(GET_REPOSITORY, {
+    variables: { id },
+    fetchPolicy: 'cache-and-network',
+  })
+
+  if (loading) return null;
+  if (error) return <Text>Error: {error.message}</Text>
+
+  const repository = data.repository
+  const reviews = repository.reviews?.edges?.map(edge => edge.node) ?? []
+
+  return (
+    <FlatList
+      data={reviews}
+      renderItem={({ item }) => <ReviewItem review={item} />}
+      keyExtractor={({ id }) => id}
+      ListHeaderComponent={() => <RepositoryItem item={repository} showButton />}
+      ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
+    />
+  )
+}
+
+export default RepositoryView
